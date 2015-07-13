ea4a31e915c46a6914b5e34fd8ec0b5ef3925ec8
Eugene <yeo.eugene.oey@gmail.com>
2015-07-14 07:50:28 +0800

    use http

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://eugene.gixs.work'
+SITEURL = 'http://eugene.gixs.work'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
